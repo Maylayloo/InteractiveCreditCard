98839e68ac8bdfae0b47dfe002df51c84b203375
Maylayloo <milosz.l-24@wp.pl>
2022-09-27 00:13:15 +0200

    begin of scripting and design improving

--- app.js
+++ app.js
@@ -12,14 +12,46 @@ let onCardExpiryYear = document.getElementById("expiry-year")
 
 let cvcInput = document.getElementById("cvc-input");
 let onCardCVC = document.getElementById("cvc");
+let cvcErrorMessage = document.getElementById("cvc-error");
 
 let confirmButton = document.getElementById("confirm-button");
+
+function errorHandlingForExpAndCVC(errorMessage, input, onCardData, inputMaxLength, baseValue) {
+     errorMessage.style.display = "none";
+
+
+     if (input.value.length === 3 && !isNaN(input.value) && !/\s/.test(input.value))
+          onCardData.innerHTML = input.value;
+
+     else {
+          errorMessage.style.display = "block";
+          onCardCVC.innerHTML = baseValue;
+
+          if (input.value === "") {
+               errorMessage.innerHTML = "Can't be blank";
+          }
+
+          else if (input.value.length !== inputMaxLength)
+               errorMessage.innerHTML = "Wrong format, 3 digits";
+
+          else if (isNaN(input.value))
+               errorMessage.innerHTML = "Wrong format, numbers only";
+
+          else if (/\s/.test(input.value))
+               errorMessage.innerHTML = "Can't contain spaces";
+
+          input.value = "";
+     }
+}
+
+
 
 confirmButton.addEventListener("click", function() {
      onCardCardholder.innerHTML = cardholderInput.value.toUpperCase();
      onCardCardNumber.innerHTML = cardNumberInput.value;
      onCardExpiryMonth.innerHTML = expiryMonthInput.value;
      onCardExpiryYear.innerHTML = expiryYearInput.value;
-     onCardCVC.innerHTML = cvcInput.value;
+
+     errorHandlingForExpAndCVC(cvcErrorMessage, cvcInput, onCardCVC, 3, "000");
 
 })
