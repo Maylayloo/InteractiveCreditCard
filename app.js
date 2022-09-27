1bd04cec855976eb020015016919bb6a5f83476c
Maylayloo <milosz.l-24@wp.pl>
2022-09-27 21:56:28 +0200

    more logic and some errors handling

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 let cardholderInput = document.getElementById("cardholder-name-input");
 let onCardCardholder = document.getElementById("cardholder-name");
+let cardHolderErrorMessage = document.getElementById("cardholder-error");
 
 let cardNumberInput = document.getElementById("card-number-input");
 let onCardCardNumber = document.getElementById("card-number");
+let cardNumberErrorMessage = document.getElementById("card-number-error");
 
 let expiryMonthInput = document.getElementById("expiry-date-month-input");
 let onCardExpiryMonth = document.getElementById("expiry-month");
 
 let expiryYearInput = document.getElementById("expiry-date-year-input");
 let onCardExpiryYear = document.getElementById("expiry-year")
+let expiryErrorMonthMessage = document.getElementById("expiry-error-month");
+let expiryErrorYearMessage = document.getElementById("expiry-error-year");
 
 let cvcInput = document.getElementById("cvc-input");
 let onCardCVC = document.getElementById("cvc");
@@ -16,16 +20,29 @@ let cvcErrorMessage = document.getElementById("cvc-error");
 
 let confirmButton = document.getElementById("confirm-button");
 
+let rightSection = document.getElementById("right-section");
+let acceptedSection = document.getElementById("acceptedSection");
+
+let isOkay = false;
+
+function isInputValueBlank(input, errorMessage) {
+     if (input.value.replaceAll(" ", "") === "") {
+          errorMessage.style.display = "block";
+          errorMessage.innerHTML = "Can't be blank";
+     }
+}
+
 function errorHandlingForExpAndCVC(errorMessage, input, onCardData, inputMaxLength, baseValue) {
-     errorMessage.style.display = "none";
-
-
-     if (input.value.length === 3 && !isNaN(input.value) && !/\s/.test(input.value))
+     if (input.value.length === inputMaxLength && !isNaN(input.value) && !/\s/.test(input.value)) {
+          errorMessage.style.display = "none";
           onCardData.innerHTML = input.value;
+          isOkay = true;
+     }
+
 
      else {
           errorMessage.style.display = "block";
-          onCardCVC.innerHTML = baseValue;
+          onCardData.innerHTML = baseValue;
 
           if (input.value === "") {
                errorMessage.innerHTML = "Can't be blank";
@@ -48,10 +65,31 @@ function errorHandlingForExpAndCVC(errorMessage, input, onCardData, inputMaxLeng
 
 confirmButton.addEventListener("click", function() {
      onCardCardholder.innerHTML = cardholderInput.value.toUpperCase();
-     onCardCardNumber.innerHTML = cardNumberInput.value;
+
+     let temporaryCardNumber = cardNumberInput.value.replaceAll(" ", "");
+     let formattedCardNumber = "";
+     for (let i = 0; i <= 12; i+=4) {
+          formattedCardNumber += temporaryCardNumber.slice(i, i+4) + " ";
+     }
+
+     onCardCardNumber.innerHTML = formattedCardNumber;
+
      onCardExpiryMonth.innerHTML = expiryMonthInput.value;
      onCardExpiryYear.innerHTML = expiryYearInput.value;
 
      errorHandlingForExpAndCVC(cvcErrorMessage, cvcInput, onCardCVC, 3, "000");
+
+     isInputValueBlank(cardholderInput, cardHolderErrorMessage);
+     isInputValueBlank(cardNumberInput, cardNumberErrorMessage);
+
+     if (isOkay)
+     {
+          acceptedSection.style.opacity = '1';
+          acceptedSection.style.zIndex = '15';
+          rightSection.style.zIndex = '-1';
+          rightSection.style.opacity = '0';
+     }
+
+
 
 })
